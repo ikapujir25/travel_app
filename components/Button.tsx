97e194a8e19cd86e93addbffa496c4f533c8bf0b
Ika Puji Rahayu <ikapujirahayu25@gmail.com>
2024-11-28 15:08:41 +0700

    feat: Slicing Button Component

---
+++ components/Button.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import React from "react";
+
+type ButtonProps = {
+  type: "button" | "submit";
+  title: string;
+  icon?: string;
+  variant: "btn_dark_green";
+};
+
+const Button = ({ type, title, icon, variant }: ButtonProps) => {
+  return (
+    <button className={`flexCenter gap-3 rounded-full ${variant}`} type={type}>
+      {icon && <Image src={icon} alt={icon} width={24} height={24} />}
+      <label htmlFor="" className="bold-16 whitespace-nowrap">
+        {title}
+      </label>{" "}
+    </button>
+  );
+};
+
+export default Button;
